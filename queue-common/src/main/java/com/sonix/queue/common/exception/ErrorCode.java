67ef50376ad968d6c1dff39eea0b3d4bbcf052cf
S-Onix <msyoo101@hansung.ac.kr>
2026-04-23 16:06:58 +0900

    feat: error code add

--- queue-common/src/main/java/com/sonix/queue/common/exception/ErrorCode.java
+++ queue-common/src/main/java/com/sonix/queue/common/exception/ErrorCode.java
@@ -2,7 +2,10 @@ package com.sonix.queue.common.exception;
 
 public enum ErrorCode {
 
-    INTERNAL_SERVER_ERROR("I004", "서버 오류가 발생했습니다.", 500);
+    INTERNAL_SERVER_ERROR("I004", "서버 오류가 발생했습니다.", 500),
+    DUPLICATE_EMAIL("T001", "이미 존재하는 이메일입니다.", 409),
+    TENANT_NOT_FOUND("T002", "Tenant를 찾을 수 없습니다.", 404),
+    INVALID_PASSWORD("T003", "비밀번호가 일치하지 않습니다.", 401);
 
     private final String code;
     private final String message;
