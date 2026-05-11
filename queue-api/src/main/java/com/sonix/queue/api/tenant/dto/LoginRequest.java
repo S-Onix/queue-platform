5f80bfa024eaa3d03683e11adb7d9bf658d189bb
S-Onix <msyoo101@hansung.ac.kr>
2026-05-11 17:54:44 +0900

    feat: Tenant Login + Token refresh

---
+++ queue-api/src/main/java/com/sonix/queue/api/tenant/dto/LoginRequest.java
@@ -0,0 +1,14 @@
+package com.sonix.queue.api.tenant.dto;
+
+import jakarta.validation.constraints.NotBlank;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class LoginRequest {
+    @NotBlank
+    private String email;
+    @NotBlank
+    private String password;
+}
---
+++ queue-api/src/main/java/com/sonix/queue/api/tenant/dto/RefreshRequest.java
@@ -0,0 +1,12 @@
+package com.sonix.queue.api.tenant.dto;
+
+import jakarta.validation.constraints.NotBlank;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class RefreshRequest {
+    @NotBlank
+    String token;
+}
