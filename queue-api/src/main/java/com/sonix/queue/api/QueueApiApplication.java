200ebce76f6515dacd98c411102b0d25a32e641f
S-Onix <msyoo101@hansung.ac.kr>
2026-05-11 16:38:30 +0900

    feat: application start & Virtual Thread activation

---
+++ queue-api/src/main/java/com/sonix/queue/api/QueueApiApplication.java
@@ -0,0 +1,12 @@
+package com.sonix.queue.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication(scanBasePackages = "com.sonix.queue")
+
+public class QueueApiApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(QueueApiApplication.class, args);
+    }
+}
