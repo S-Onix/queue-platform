8a2b23ac1294ca182b2e3bc95a736ce4818f7d88
Sonix <msyu27@gmail.com>
2026-07-23 22:45:29 +0900

    feat: batch consumer insert to token Table check

--- queue-batch/src/main/java/com/sonix/queue/batch/token/TokenEnqueueService.java
+++ queue-batch/src/main/java/com/sonix/queue/batch/token/TokenEnqueueService.java
@@ -1,4 +1,22 @@
 package com.sonix.queue.batch.token;
 
+import com.sonix.queue.domain.queue.Token;
+import com.sonix.queue.domain.queue.TokenRepository;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+@Service
 public class TokenEnqueueService {
+    private final TokenRepository tokenRepository;
+
+    public TokenEnqueueService(TokenRepository tokenRepository) {
+        this.tokenRepository = tokenRepository;
+    }
+
+    @Transactional
+    public void append(List<Token> tokens){
+        tokenRepository.saveAllIfAbsent(tokens);
+    }
 }
